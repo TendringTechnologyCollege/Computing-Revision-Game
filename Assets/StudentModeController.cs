8c66ec7e10163d29fcd0058c7eea9e4235d40c21
Joe Rackham <joerackhan@hotmail.co.uk>
2016-11-21 10:14:34 +0000

    ReadMe.md
    This is my revision game, version 1.0

---
+++ Assets/StudentModeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StudentModeController : MonoBehaviour {
+
+	private Player player;
+
+	class Character {
+		private int health;
+		private Sprite characterSprite;
+		private GameObject instance;
+		public void newCharacter(Sprite sprite) {
+			health = 100;
+			characterSprite = sprite;
+			instance;
+
+		}
+	}
+
+	class Player {
+
+	}
+
+	class Quiz {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
