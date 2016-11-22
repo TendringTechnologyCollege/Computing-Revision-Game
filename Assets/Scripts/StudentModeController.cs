61e8b0eecb1c0f56b5e5abdbda8bb547fff404bf
Joe Rackham <joerackhan@hotmail.co.uk>
2016-11-22 10:58:31 +0000

    Student Class Continued
    Adding Move and Deativate Subroutines to Player class

--- Assets/Scripts/StudentModeController.cs
+++ Assets/Scripts/StudentModeController.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class StudentModeController : MonoBehaviour {
 
+	public Sprite playerSprite;
+
 	private Player player;
 
 	class Character {
 		private int health;
 		private Sprite characterSprite;
 		private GameObject instance;
-		public Character(Sprite sprite) {
+		private Rigidbody rigidbody;
+		private SpriteRenderer spriteRenderer;
+		public Character(Sprite sprite, string name) {
 			health = 100;
 			characterSprite = sprite;
-			instance = new GameObject("Hello World");
+			instance = new GameObject(name);
 			instance.AddComponent<SpriteRenderer> ();
+			instance.AddComponent<Rigidbody> ();
+			rigidbody = instance.GetComponent<Rigidbody>();
+			rigidbody.isKinematic = true;
+			rigidbody.useGravity = false;
+			spriteRenderer =  instance.GetComponent<SpriteRenderer>();
+			spriteRenderer.sprite = characterSprite;
 		}
 		public void setHealth(int currentHealth) {
 			health = currentHealth;
@@ -21,16 +31,18 @@ public class StudentModeController : MonoBehaviour {
 		public int getHealth() {
 			return health;
 		}
-		public void move() {
-			
+		public void move(float x, float y) {
+			rigidbody.position = new Vector3 (x, y, 0f);
 		}
 		public void deactivate() {
-			Destroy (instance);
+			instance.SetActive (false);
 		}
 	}
 
 	class Player : Character {
-		public void 
+		public Player (Sprite sprite, string name) : base(sprite,name) {
+			
+		}
 	}
 
 	class Quiz {
@@ -39,7 +51,7 @@ public class StudentModeController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Character character = new Character ();
+		Player player = new Player (playerSprite,"Joe");
 	}
 	
 	// Update is called once per frame
